e76cba134e7f216929472a036a5c2cbe7149cb3e
John Naulty <jnaulty@gmail.com>
2022-11-06 01:58:50 -0700

    update key.rs component

--- src/components/atoms/key.rs
+++ src/components/atoms/key.rs
@@ -1,22 +1,26 @@
+use gloo::console::log;
 use yew::prelude::*;
 use crate::User;
+use crate::components::atoms::main_title::{Color, MainTitle};
 
 
 #[function_component(Key)]
 pub fn key() -> Html {
+
+    let main_title_load = Callback::from(|message: String| log!(message));
     let user_context: Option<User> = use_context::<User>();
     let key = user_context.clone().unwrap_or_default().key;
     let key_message = "User Private PGP Key";
     if user_context.clone().unwrap_or_default().key_submit {
     html! {
         <>
-        <h2>{key_message}</h2>
+        <MainTitle title={key_message} color={Color::Ok} on_load={&main_title_load}/>
         <p><pre>{key}</pre></p>
         </>
     } 
 } else {
     html! {
-        <h2>{"Key Not Created Yet"}</h2>
+        <MainTitle title="Key Not Created Yet" color={Color::Error} on_load={&main_title_load}/>
     }
 }
 }
